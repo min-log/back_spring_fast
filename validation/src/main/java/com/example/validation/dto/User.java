7e3b84368082f614891df4bc4f4a6f8aa5b49b66
min-log <minlog.0430@gmail.com>
2023-03-09 14:51:42 +0900

    validation 공부 add

--- validation/src/main/java/com/example/validation/dto/User.java
+++ validation/src/main/java/com/example/validation/dto/User.java
@@ -1,8 +1,13 @@
 package com.example.validation.dto;
 
+import com.example.validation.anotation.YearMonth;
+
 import javax.validation.constraints.*;
 
+
 public class User {
+
+
     @NotBlank(message = "이름을 확인해주세요.")
     private String name;
     @Min(value = 10, message = "나이를 확인해주세요.")
@@ -12,6 +17,12 @@ public class User {
     @Pattern(regexp = "^\\d{2,3}-\\d{3,4}-\\d{4}$",message = "핸드폰 번호 양식과 맞지 않습니다. ex:010-0000-0000")
     private String phoneNumber;
 
+    //날짜를 받는 필드
+   // @Size(min=6,max = 6,message = "날짜를 정확하게 입력해주세요 ex ) yyyyMM")
+    @YearMonth(pattern="yyyyMM")
+    private String reqYearMonth;
+
+
     @Override
     public String toString() {
         return "User{" +
@@ -19,6 +30,7 @@ public class User {
                 ", age=" + age +
                 ", email='" + email + '\'' +
                 ", phoneNumber='" + phoneNumber + '\'' +
+                ", reqYearMonth='" + reqYearMonth + '\'' +
                 '}';
     }
 
@@ -52,5 +64,13 @@ public class User {
 
     public void setPhoneNumber(String phoneNumber) {
         this.phoneNumber = phoneNumber;
+    }
+
+    public String getReqYearMonth() {
+        return reqYearMonth;
+    }
+
+    public void setReqYearMonth(String reqYearMonth) {
+        this.reqYearMonth = reqYearMonth;
     }
 }

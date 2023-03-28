065cbd661bad2c5396144e69851bf8bcbdfbc491
jimin <minlog.0430@gmail.com>
2023-03-28 17:44:44 +0900

    테스트

--- delete/src/main/java/com/example/delete/controller/DeleteController.java
+++ delete/src/main/java/com/example/delete/controller/DeleteController.java
@@ -9,7 +9,7 @@ public class DeleteController {
     public void delete(@PathVariable String userId, @RequestParam String account){
         System.out.println(userId);
         System.out.println(account);
-
+        
         //delete 리소스 삭제, 제거할 값이 이미 없어도 동일하게 200 ok를 반환한다.
     }
 
